07ab5ec0dd22fb9708ba94f287ee7f33722a5398
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-04-20 14:17:47 +0900

    fix: 프로젝트 수준 build.gradle plugin 추가
    ksp version downgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,11 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.parcelize) apply false
     alias(libs.plugins.hilt.android) apply false
+    alias(libs.plugins.kotlinx.serialization) apply false
     alias(libs.plugins.kotlin.kapt) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.secrets.gradle.plugin) apply false
 }
