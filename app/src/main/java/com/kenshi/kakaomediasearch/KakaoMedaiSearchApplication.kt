65232f57b3e2dc6955e82037c54d48cb2736c7ea
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-05-06 13:35:28 +0900

    feat: Timber setup

--- app/src/main/java/com/kenshi/kakaomediasearch/KakaoMedaiSearchApplication.kt
+++ app/src/main/java/com/kenshi/kakaomediasearch/KakaoMedaiSearchApplication.kt
@@ -2,6 +2,18 @@ package com.kenshi.kakaomediasearch
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class KakaoMedaiSearchApplication: Application()
+class KakaoMedaiSearchApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        initTimber()
+    }
+
+    private fun initTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
